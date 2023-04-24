a99b4ed5610a038fe61724702c709829dd483c05
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-04-24 13:42:02 -0700

    Add 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/comm.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determine if an number is positive, negative or Zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
