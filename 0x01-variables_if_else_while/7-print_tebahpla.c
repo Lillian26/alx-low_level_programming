7093b8491143ded020602b5580d6951b08d9474b
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-04-24 15:14:33 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determine if an number is positive, negative or Zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	return (0);
+}
