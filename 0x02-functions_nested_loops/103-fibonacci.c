2f9994f227777df6aa747387520f2cbbef10aa02
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-04-26 03:30:02 -0700

    Add 103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,17 +8,16 @@
  */
 int main(void)
 {
-	long int i, fib1 = 1, fib2 = 2, fib3;
+	long int i, fib1 = 1, fib2 = 2, fib3, sum = 3;
 
-	printf("%ld, %ld", fib1, fib2);
 	for (i = 2; i < 50; ++i)
 	{
 		fib3 = fib1 + fib2;
 		if (fib3 < 4000000 && (fib3 % 2) == 0)
-			printf(", %ld", fib3);
+			sum += fib3;
 		fib1 = fib2;
 		fib2 = fib3;
 	}
-	printf("\n");
+	printf("%ld\n", sum);
 	return (0);
 }
