5302a173437ab8679fead71e05d1f467863a52f3
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-04-26 03:17:19 -0700

    Add 103-fibonacci even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, fib1 = 1, fib2 = 2, fib3;
+
+	printf("%ld, %ld", fib1, fib2);
+	for (i = 2; i < 50; ++i)
+	{
+		fib3 = fib1 + fib2;
+		if (fib3 < 4000000 && (fib3 % 2) == 0)
+			printf(", %ld", fib3);
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	printf("\n");
+	return (0);
+}
