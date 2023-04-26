fcc7b0583f9ecbbe78fcd7dc74d045104b5ae240
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-04-25 17:48:57 -0700

    Add 2-print_aplhabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
