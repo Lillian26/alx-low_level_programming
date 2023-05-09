6dcb6998a2f8bd46b16cc934eb0da27b3eccf3ad
Lillian Namusisisi <lilliannamusisi@gmail.com>
2023-05-09 12:01:24 -0700

    Correct betty warnings.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and 
+ * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98
  *
  * @n: the pointer to be used
@@ -10,6 +10,7 @@
  * Return 0
  */
 
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+{
 	*n = 98;
 }
