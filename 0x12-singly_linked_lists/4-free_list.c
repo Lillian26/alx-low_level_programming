42b73f498e8adec30761a45a6504bd2363157551
Lillian Namusisi <lilliannamusisi@gmail.com>
2023-06-05 07:49:27 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - fress a list
+ * @head: pointer to the head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next, *current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
