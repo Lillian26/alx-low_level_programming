2533946f015420f4f45c326129bd55b205d85f64
Lillian Namusisi <lilliannamusisi@gmail.com>
2023-06-06 06:58:35 -0700

    Add 9-insert_nodeint.c 5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head
 		*head = n_node;
 		return (n_node);
 	}
-	while (t_node != NULL && idx < count)
+	while (t_node != NULL && count < idx)
 	{
 		if (count == idx - 1)
 		{
